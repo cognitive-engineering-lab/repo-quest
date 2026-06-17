69e5b648884166c418dee310c2288ef29eecb221
Will Crichton <wcrichto@cs.stanford.edu>
2026-06-16 21:38:21 -0400

    Add help info and daemon handling to startup script

--- run-repo-quest.sh
+++ run-repo-quest.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
+if ! docker info >/dev/null 2>&1; then
+  echo "Error: could not connect to Docker daemon. You might need to start Docker and run this command again."
+  exit 1
+fi
+
 DOCKER_HOST=$(docker context inspect --format '{{.Endpoints.docker.Host}}')
 export RQ_DOCKER_HOST=${DOCKER_HOST#unix://}
 docker compose --file oci://ghcr.io/cognitive-engineering-lab/repoquest-compose:latest up --detach --yes
+echo "RepoQuest is now up and running here: http://localhost:8085"
+echo "To stop the process, run:"
+echo "  docker compose --file oci://ghcr.io/cognitive-engineering-lab/repoquest-compose:latest down"
 open http://localhost:8085
